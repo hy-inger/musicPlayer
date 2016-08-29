3c06a3cb599fde00250fdf53a62bae63a232b15f
wangquan <wangquan@youmi.net>
2016-08-29 10:20:29 +0800

    改进进度条

--- src/main.js
+++ src/main.js
@@ -30,16 +30,52 @@ $q("#player").on("pause", function(){
     $q("button[name='play']").removeClass("play-btn").addClass("pause-btn");
 });
 
+// $q("#player").on('durationchange', function(){
+//     $q("#duration").text(parseInt(this.duration));
+//     // console.log(this.duration);
+// });
 
 $q("#player").on('timeupdate', function(){
-    var progress = (this.currentTime/this.duration)*1000;
-    $q(".time-line").val(progress);
-    $q(".played-progress").css('width', progress/10+'%')
+    var progress = (this.currentTime/this.duration)*100;
+    $q(".played-progress").css('width',progress+'%');
+    // $q("#current-time").text(parseInt(this.currentTime));
 });
 
-$q(".time-line").on('input',function () {
-    // Dispatch("CHANGE_CURRENT_TIME", $q(this).val()/10);
-    $q("#player")[0].currentTime = $q("#player")[0].duration*$q(this).val()/1000;
-    $q(".played-progress").css('width', $q(this).val()/10*0.99+'%')
+var clickPos = (function(){
+    var length = $q(".progress-container")[0].offsetWidth,
+        left = $q(".progress-container")[0].offsetLeft;
+    return function(pos){
+        return (pos-left)/length;
+    }
+})();
+
+$q(".progress-container").on('click',function (e) {
+    var progress = clickPos(e.clientX);
+    $q("#player")[0].currentTime = parseInt($q("#player")[0].duration*progress);
+    $q(".played-progress").css('width',progress*100+'%');
 });
 
+var isHold = false;
+
+$q(".slider").on('mousedown', function(){
+    isHold = true;
+    console.log(isHold);
+});
+
+
+$q(".progress-container").on('mouseup', function(){
+    isHold = false;
+    console.log(isHold);
+});
+
+$q(".player-control-bar").on('mousemove', function(e){
+    if(!isHold) return;
+    var progress = clickPos(e.clientX);
+    if(progress >= 1){
+        progress = 1;
+    }else if(progress<=0){
+        progress = 0;
+    }
+    $q("#player")[0].currentTime = parseInt($q("#player")[0].duration*progress);
+    $q(".played-progress").css('width',progress*100+'%');
+});
