2f88f2cd7d578cc168573a3fe8ba6b00e8c20b5b
zhengzhirong <zhengzhirong@youmi.net>
2016-08-29 09:35:14 +0800

    server提供歌词接口.将音乐加载合并.修复bug

--- src/js/ctrl.js
+++ src/js/ctrl.js
@@ -1,153 +1,168 @@
-
 /**
  * 主要用来对播放器和音乐的控制
  * 通过调用Dispatch(action)来控制播放器进行相应的操作
  * 不允许直接对播放器对象进行操作
  */
-var {MyAudio,Music} = require("./audio.js");
+var { MyAudio, Music } = require("./audio.js");
 require("expose?$q!./query.js");
 // 创建一首音乐并且将音乐与其dom关联.
-function createMusicDom(music){
-    var index = $q(".song-list li").length;
-    var li = `<li><span>${index}</span><span>${music.songName}</span><i class="rm">x</i></li>`;
-    var dom = $q.parseHTML(li);
-    var mus = new Music(music,dom);
-    $q(dom).on('click', function(event) {
-        event.preventDefault();
-        window.player.load(mus);
-    });
-    $q(dom).find(".rm").on('click', function(event) {
-        event.stopPropagation();
-        Dispatch("REMOVE_MUSIC",mus,(rm,list)=>{
-            $q(rm.dom).remove();
-        });
-    });
-    return mus;
-}
+
 var Store = {
     // 状态,存放播放器和搜索列表
-    state : {
-        player:new MyAudio(document.getElementById("player")), // 播放器实例
-        searchList:[]  // 当前的搜索列表
+    state: {
+        player: new MyAudio(document.getElementById("player")), // 播放器实例
+        searchList: [] // 当前的搜索列表
     },
     // 相应的操作事件
-    mou : {
+    mou: {
         // 播放
-        PLAY(state){
+        PLAY(state) {
             state.player.play();
         },
         // 暂停
-        PAUSE(state){
+        PAUSE(state) {
             state.player.pause();
         },
         // 搜索
-        SEARCH(state,query,callback){
+        SEARCH(state, query, callback) {
             // 向后台拉取搜索到的数据
-            fetch("http://localhost:4000/search?query="+query,{
-                mod:"cors"
-            }).then(res=>res.json()).then(data=>{
+            fetch("http://localhost:4000/search?query=" + query, {
+                mod: "cors"
+            }).then(res => res.json()).then(data => {
                 callback(data.data.songList);
             });
         },
         // 刷新搜索列表,即将搜索的歌曲重新放入列表当中
-        REFRESH_SEARCH_LIST(state,search_list){
+        REFRESH_SEARCH_LIST(state, search_list) {
             state.searchList = search_list;
         },
         // 播放/暂停切换
-        CHANGE_STATE(state){
+        CHANGE_STATE(state) {
             if (state.player.audio.paused) {
                 state.player.play();
-            }else{
+            } else {
                 state.player.pause();
             }
         },
-        // 下一首歌曲
-        NEXT(state){
-            state.player.next();
+        /**
+         * 播放音乐,并将音乐通过回调传递
+         * @param {[Object]}   state    [状态]
+         * @param {[Music|String]}   music    音乐对象或者'next'和'prev'字符串,代表上一首或者下一首
+         * @param {Function} callback [执行回调,参数musci]
+         */
+        LOAD_MUSIC(state, music, callback) {
+            if (music === 'next') {
+                music = state.player.next();
+            } else if (music === 'prev') {
+                music = state.player.prev();
+            } else {
+                state.player.load(music);
+            }
+            callback && callback(music);
         },
-        // 上一首歌曲
-        PREV(state){
-            state.player.prev();
-        },
-        // 从搜索列表中播放音乐
-        LOAD_FROM_SEARCHLIST(state,index,callback){
-            var music = state.searchList[index];
-            music = createMusicDom(music);
-            state.player.load(music);
+
+        // 添加音乐，提供回调给main进行dom操作
+        ADD_MUSIC(state, index, callback) {
+            var music = new Music(state.searchList[index]);
+            // 根据songId寻找该歌曲是否已经在播放列表
+            // 如果在,则跳转至该歌曲播放
+            // 如果不在则添加该歌曲
+            /**
+             * Array.findIndex 返回元素所在的索引,如果不在则返回-1
+             * 回调function(element,index,array){}
+             * 如果回调中返回true,则Array.findIndex会返回该元素所在的索引
+             * 如果回调返回fasle,会继续寻找下一个,
+             */
+            var player = state.player;
+            var index = player.musicList.findIndex((ele, i, arr) => {
+                if (ele.info.songId === music.info.songId) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+            if (index === -1) {
+                player.musicList.push(music);
+                player.index = player.musicList.length - 1;
+            } else {
+                player.index = index;
+                music = player.musicList[index];
+            }
+            // 执行回调
             callback(music);
         },
         // 加大音量,基数为10
-        INCREASE_VOLUME(state){
+        INCREASE_VOLUME(state) {
             state.player.changeVolume('increase');
         },
         // 减少音量,基数为10
-        DECREASE_VOLUME(state){
+        DECREASE_VOLUME(state) {
             state.player.changeVolume('decrease');
         },
         // 改变音量音量
-        CHANGE_VOLUME(state,val){
+        CHANGE_VOLUME(state, val) {
             state.player.changeVolume(val);
         },
         // 播放进度,0-100整数,代表百分比
-        CHANGE_CURRENT_TIME(val){
+        CHANGE_CURRENT_TIME(state,val) {
             state.player.setCurrentTime(val);
         },
         // 播放模式,mod为将要修改的模式
         // order : 顺序播放
         // range : 随机播放
         // loop : 单曲循环
-        CHANGE_PLAY_MOD(state,mod){
+        CHANGE_PLAY_MOD(state, mod) {
             if (mod === 'loop') {
                 state.player.audio.loop = true;
-            }else{
+            } else {
                 state.player.audio.loop = false;
                 state.player.cfg.playMod = mod;
             }
         },
         // 获取当前播放列表
         // 返回 Array.
-        GET_MUSIC_LIST(state){
+        GET_MUSIC_LIST(state) {
             return state.player.musicList;
         },
         // 删除某一首音乐
         // 如果music为number,则删除该索引的音乐
         // 如果music为Music实例,则删除该对象
-        REMOVE_MUSIC(state,music,fnc){
+        REMOVE_MUSIC(state, music, fnc) {
             let player = state.player;
-            let remove ,index;
+            let remove, index;
             if (typeof music === 'number') {
                 index = music;
-            }else {
+            } else {
                 index = player.musicList.indexOf(music);
             }
             if (index === player.index) {
                 player.next();
             }
-            index <= player.index&&player.index--;
-            remove = player.musicList.splice(index,1);
+            index <= player.index && player.index--;
+            remove = player.musicList.splice(index, 1);
             if (typeof fnc === 'function') {
-                fnc(...remove,player.musicList);
+                fnc(...remove, player.musicList);
             }
         },
-       /**
-        * 获取歌曲歌词接口
-        * @param {state} state 管理的状态,自动注入
-        * @param {Music} music music对象,会根据对象的songId获取相应的歌词
-        * @return {Objec} 异步获取的data.
-        *         data.lrcContent : 歌词
-        *         data.title : 歌名
-        */
-        GET_MUSIC_LRC(state,music){
+        /**
+         * 获取歌曲歌词接口
+         * @param {state} state 管理的状态,自动注入
+         * @param {Music} music music对象,会根据对象的songId获取相应的歌词
+         * @return {Objec} 异步获取的data.
+         *         data.lrcContent : 歌词
+         *         data.title : 歌名
+         */
+        GET_MUSIC_LRC(state, music, callback) {
             var songId = music.info.songId;
-            fetch("http://localhost:4000/lrc?songId="+songId,{
-                mod:"cors"
-            })
-            .then((res)=>res.json())
-            .then((data)=>{
-                // data.lrcContent : 歌词
-                // data.title  : 歌名
-                console.log(data);
-            });
+            fetch("http://localhost:4000/lrc?songId=" + songId, {
+                    mod: "cors"
+                })
+                .then((res) => res.json())
+                .then((data) => {
+                    // data.lrcContent : 歌词
+                    // data.title  : 歌名
+                    callback(data);
+                });
         }
     }
 };
@@ -155,14 +170,14 @@ var Store = {
  * 自执行函数,返回一个方法
  * @type {RegExp}
  */
-var Dispatch = (function(store){
+var Dispatch = (function(store) {
     /**
      * 执行action操作,arg为参数
      */
-    return function(action,...arg){
-        console.log("ACTION-------->>>",action);
+    return function(action, ...arg) {
+        console.log("ACTION-------->>>", action);
         // 调用store中mou对应的action
-        return store.mou[action](store.state,...arg);
+        return store.mou[action](store.state, ...arg);
     };
 })(Store);
-module.exports = {Store,Dispatch};
+module.exports = { Store, Dispatch };
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ require("./sass/style.scss");
 var { Store, Dispatch } = require("./js/ctrl.js");
 window.player = Store.state.player;
 Dispatch("SEARCH", "陈奕迅", function(musicList) {
+    // 创建搜索列表
     createSearchList(musicList);
 });
 $q("button[name='play']").on('click', function(event) {
@@ -13,11 +14,11 @@ $q("button[name='play']").on('click', function(event) {
 $q("button[changebtn]").on('click', function(event) {
     var type = $q(this).attr("action-type");
     if (type === 'next') {
-        // 分发下一首音乐事件
-        Dispatch("NEXT");
+        // 分发下一首音乐事件,并使用回调改变播放列表中的active状态
+        Dispatch("LOAD_MUSIC","next",playMusic);
     } else {
-        // 分发上一首音乐事件
-        Dispatch("PREV");
+        // 分发上一首音乐事件,并使用回调改变播放列表中的active状态
+        Dispatch("LOAD_MUSIC","prev",playMusic);
     }
 });
 $q("#search-btn").on('click', function(event) {
@@ -42,10 +43,11 @@ $q("#player").on('timeupdate', function() {
 });
 
 $q(".time-line").on('input', function() {
-    // Dispatch("CHANGE_CURRENT_TIME", $q(this).val()/10);
-    $q("#player")[0].currentTime = $q("#player")[0].duration * $q(this).val() / 1000;
+    Dispatch("CHANGE_CURRENT_TIME", $q(this).val()/10);
+    // $q("#player")[0].currentTime = $q("#player")[0].duration * $q(this).val() / 1000;
     $q(".played-progress").css('width', $q(this).val() / 10 * 0.99 + '%');
 });
+
 
 // 根据获取的音乐数据构造音乐列表
 function createSearchList(musicList) {
@@ -61,14 +63,59 @@ function createSearchList(musicList) {
     $q(".search-list tbody tr").on('dblclick', function(event) {
         var index = $q(this).data("index");
         // 从搜索列表中添加音乐到播放列表并播放
-        Dispatch("LOAD_FROM_SEARCHLIST", index, function(music) {
-            $q(".song-list")[0].appendChild(music.dom);
-            // 获取歌词
-            Dispatch('GET_MUSIC_LRC', music);
+        Dispatch("ADD_MUSIC", index, function(music) {
+            // 判断音乐是否已存在dom，存在dom说明是已经存在于播放列表当中，不存在dom则为新增歌曲
+            if(!music.dom){
+                // 添加并绑定dom
+                createMusicDom(music);
+            }
+            // 改变当前播放列表的active状态
+            Dispatch('LOAD_MUSIC',music,playMusic);
         });
     }).on("click", function() {
         $q(".search-list tbody tr").removeClass('active');
         // 从搜索列表单击只能触发选中状态
         $q(this).addClass('active');
+    });
+}
+
+
+// 为新加入到播放列表的音乐创建和绑定dom
+function createMusicDom(music){
+    var index = $q(".song-list li").length;
+    var li = `<li><span>${index}</span><span>${music.info.songName}</span><i class="rm">x</i></li>`;
+    var dom = $q.parseHTML(li);
+    music.dom = dom;
+    $q(dom).on('click', function(event) {
+        event.preventDefault();
+        window.player.load(music);
+    });
+    $q(dom).find(".rm").on('click', function(event) {
+        event.stopPropagation();
+        Dispatch("REMOVE_MUSIC",music,(rm,list)=>{
+            $q(rm.dom).remove();
+        });
+    });
+     $q(".song-list")[0].appendChild(dom);
+}
+
+
+
+/**
+ * 切换当前播放音乐时候播放列表中进行的dom操作
+ * 包括添加删除active状态,获取并处理lrc歌词
+ * @param  {[type]} music [歌曲对象]
+ * @return {[type]}       [description]
+ */
+function playMusic(music){
+    $q('.song-list li').removeClass('active');
+    $q(music.dom).addClass('active');
+    var songId = music.info.songId;
+    fetch("http://localhost:4000/lrc?songId="+songId,{
+        mod:"cors"
+    })
+    .then((res)=>res.json())
+    .then((lrc)=>{
+        console.log(lrc);
     });
 }
