7383f0bf3f8259403ab9e8fd3c1d724c3f6f3d26
wangquan <wangquan@youmi.net>
2016-09-08 15:02:15 +0800

    重写底部工具栏样式和加入站位图片

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ window.player = Store.state.player;  // 仅提供给控制台查看对象
 Dispatch('SET_CONFIG',{
     ended:playMusic
 });
-Dispatch("SEARCH", "东篱", function(musicList) {
+Dispatch("SEARCH", "陈奕迅", function(musicList) {
     // 创建搜索列表
     createSearchList(musicList);
 });
@@ -217,11 +217,15 @@ function playMusic(music){
     $q('.music-icon')[0].src = music.info.songPicRadio;
     $q('.music-icon').on('load', function(){
         $q('#dynamic-bg').css('background', 'url('+music.info.songPicRadio+') 50% 50% / cover no-repeat');
-        
+    }).on('error', function(){
+        $q('.music-icon')[0].src = './static/img/logo.png';
     });
-    setTimeout(function () {
-         $q('#cover-img')[0].src = music.info.songPicRadio;
-    }, 1250);
+    // setTimeout(function () {
+    //      $q('#cover-img')[0].src = music.info.songPicRadio;
+    // }, 1250);
+    $q('#cover-img').on('error', function(){
+        this.src = './static/img/logo.png';
+    });
     
     
     $q('.song-list li').removeClass('active');
