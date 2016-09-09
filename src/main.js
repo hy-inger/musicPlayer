a31b3e3ff2e453b5f408496bb148fb8b6050268d
wangquan <wangquan@youmi.net>
2016-09-09 14:51:52 +0800

    eeee

--- src/main.js
+++ src/main.js
@@ -187,6 +187,7 @@ function createSearchList(musicList) {
     Dispatch("REFRESH_SEARCH_LIST", musicList);
     // 构造音乐列表,此处应提供回调函数给main.js调用.并且操作相应的dom元素
     var tbody = "";
+    console.log(musicList);
     musicList.forEach((music, index) => {
         var idx = index++;
         tbody += `<tr data-index="${idx}"><td>${index}</td><td>${music.songName}</td><td>${music.artistName}</td><td>${music.albumName}</td><td>${timeFilter(music.time)}</td></tr>`;
